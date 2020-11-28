98d39023e8a51877ff34d641d9a1d3bb173e36a4
shijy16 <shijy16@163.com>
2020-11-28 23:17:06 +0800

    clang-format used

--- Dataflow.h
+++ Dataflow.h
@@ -9,39 +9,41 @@
 #ifndef _DATAFLOW_H_
 #define _DATAFLOW_H_
 
-#include <llvm/Support/raw_ostream.h>
-#include <map>
 #include <llvm/IR/BasicBlock.h>
 #include <llvm/IR/CFG.h>
 #include <llvm/IR/Function.h>
+#include <llvm/Support/raw_ostream.h>
+
+#include <map>
 
 using namespace llvm;
 
-///Base dataflow visitor class, defines the dataflow function
+/// Base dataflow visitor class, defines the dataflow function
 
 template <class T>
 class DataflowVisitor {
-public:
-    virtual ~DataflowVisitor() { }
+   public:
+    virtual ~DataflowVisitor() {}
 
-    /// Dataflow Function invoked for each basic block 
-    /// 
+    /// Dataflow Function invoked for each basic block
+    ///
     /// @block the Basic Block
     /// @dfval the input dataflow value
     /// @isforward true to compute dfval forward, otherwise backward
     virtual void compDFVal(BasicBlock *block, T *dfval, bool isforward) {
         if (isforward == true) {
-           for (BasicBlock::iterator ii=block->begin(), ie=block->end(); 
-                ii!=ie; ++ii) {
-                Instruction * inst = &*ii;
+            for (BasicBlock::iterator ii = block->begin(), ie = block->end();
+                 ii != ie; ++ii) {
+                Instruction *inst = &*ii;
                 compDFVal(inst, dfval);
-           }
+            }
         } else {
-           for (BasicBlock::reverse_iterator ii=block->rbegin(), ie=block->rend();
-                ii != ie; ++ii) {
-                Instruction * inst = &*ii;
+            for (BasicBlock::reverse_iterator ii = block->rbegin(),
+                                              ie = block->rend();
+                 ii != ie; ++ii) {
+                Instruction *inst = &*ii;
                 compDFVal(inst, dfval);
-           }
+            }
         }
     }
 
@@ -51,60 +53,60 @@ public:
     /// @inst the Instruction
     /// @dfval the input dataflow value
     /// @return true if dfval changed
-    virtual void compDFVal(Instruction *inst, T *dfval ) = 0;
+    virtual void compDFVal(Instruction *inst, T *dfval) = 0;
 
     ///
     /// Merge of two dfvals, dest will be ther merged result
     /// @return true if dest changed
     ///
-    virtual void merge( T *dest, const T &src ) = 0;
+    virtual void merge(T *dest, const T &src) = 0;
 };
 
 ///
 /// Dummy class to provide a typedef for the detailed result set
-/// For each basicblock, we compute its input dataflow val and its output dataflow val
+/// For each basicblock, we compute its input dataflow val and its output
+/// dataflow val
 ///
-template<class T>
+template <class T>
 struct DataflowResult {
     typedef typename std::map<BasicBlock *, std::pair<T, T> > Type;
 };
 
-/// 
-/// Compute a forward iterated fixedpoint dataflow function, using a user-supplied
-/// visitor function. Note that the caller must ensure that the function is
-/// in fact a monotone function, as otherwise the fixedpoint may not terminate.
-/// 
+///
+/// Compute a forward iterated fixedpoint dataflow function, using a
+/// user-supplied visitor function. Note that the caller must ensure that the
+/// function is in fact a monotone function, as otherwise the fixedpoint may not
+/// terminate.
+///
 /// @param fn The function
 /// @param visitor A function to compute dataflow vals
-/// @param result The results of the dataflow 
+/// @param result The results of the dataflow
 /// @initval the Initial dataflow value
-template<class T>
-void compForwardDataflow(Function *fn,
-    DataflowVisitor<T> *visitor,
-    typename DataflowResult<T>::Type *result,
-    const T & initval) {
+template <class T>
+void compForwardDataflow(Function *fn, DataflowVisitor<T> *visitor,
+                         typename DataflowResult<T>::Type *result,
+                         const T &initval) {
     return;
 }
-/// 
-/// Compute a backward iterated fixedpoint dataflow function, using a user-supplied
-/// visitor function. Note that the caller must ensure that the function is
-/// in fact a monotone function, as otherwise the fixedpoint may not terminate.
-/// 
+///
+/// Compute a backward iterated fixedpoint dataflow function, using a
+/// user-supplied visitor function. Note that the caller must ensure that the
+/// function is in fact a monotone function, as otherwise the fixedpoint may not
+/// terminate.
+///
 /// @param fn The function
 /// @param visitor A function to compute dataflow vals
-/// @param result The results of the dataflow 
+/// @param result The results of the dataflow
 /// @initval The initial dataflow value
-template<class T>
-void compBackwardDataflow(Function *fn,
-    DataflowVisitor<T> *visitor,
-    typename DataflowResult<T>::Type *result,
-    const T &initval) {
-
+template <class T>
+void compBackwardDataflow(Function *fn, DataflowVisitor<T> *visitor,
+                          typename DataflowResult<T>::Type *result,
+                          const T &initval) {
     std::set<BasicBlock *> worklist;
 
     // Initialize the worklist with all exit blocks
     for (Function::iterator bi = fn->begin(); bi != fn->end(); ++bi) {
-        BasicBlock * bb = &*bi;
+        BasicBlock *bb = &*bi;
         result->insert(std::make_pair(bb, std::make_pair(initval, initval)));
         worklist.insert(bb);
     }
@@ -128,31 +130,25 @@ void compBackwardDataflow(Function *fn,
         if (bbexitval == (*result)[bb].first) continue;
         (*result)[bb].first = bbexitval;
 
-        for (pred_iterator pi = pred_begin(bb), pe = pred_end(bb); pi != pe; pi++) {
+        for (pred_iterator pi = pred_begin(bb), pe = pred_end(bb); pi != pe;
+             pi++) {
             worklist.insert(*pi);
         }
     }
 }
 
-template<class T>
+template <class T>
 void printDataflowResult(raw_ostream &out,
                          const typename DataflowResult<T>::Type &dfresult) {
-    for ( typename DataflowResult<T>::Type::const_iterator it = dfresult.begin();
-            it != dfresult.end(); ++it ) {
-        if (it->first == NULL) out << "*";
-        else it->first->dump();
-        out << "\n\tin : "
-            << it->second.first 
-            << "\n\tout :  "
-            << it->second.second
-            << "\n";
+    for (typename DataflowResult<T>::Type::const_iterator it = dfresult.begin();
+         it != dfresult.end(); ++it) {
+        if (it->first == NULL)
+            out << "*";
+        else
+            it->first->dump();
+        out << "\n\tin : " << it->second.first
+            << "\n\tout :  " << it->second.second << "\n";
     }
 }
-
-
-
-
-
-
 
 #endif /* !_DATAFLOW_H_ */
--- LLVMAssignment.cpp
+++ LLVMAssignment.cpp
@@ -12,23 +12,19 @@
 //
 //===----------------------------------------------------------------------===//
 
-#include <llvm/Support/CommandLine.h>
-#include <llvm/IRReader/IRReader.h>
-#include <llvm/IR/LLVMContext.h>
-#include <llvm/Support/SourceMgr.h>
-#include <llvm/IR/LegacyPassManager.h>
-#include <llvm/Support/ToolOutputFile.h>
-
 #include <llvm/Bitcode/BitcodeReader.h>
 #include <llvm/Bitcode/BitcodeWriter.h>
-
-
-#include <llvm/Transforms/Utils.h>
-#include <llvm/Transforms/Scalar.h>
-
 #include <llvm/IR/Function.h>
+#include <llvm/IR/LLVMContext.h>
+#include <llvm/IR/LegacyPassManager.h>
+#include <llvm/IRReader/IRReader.h>
 #include <llvm/Pass.h>
+#include <llvm/Support/CommandLine.h>
+#include <llvm/Support/SourceMgr.h>
+#include <llvm/Support/ToolOutputFile.h>
 #include <llvm/Support/raw_ostream.h>
+#include <llvm/Transforms/Scalar.h>
+#include <llvm/Transforms/Utils.h>
 
 #include "Liveness.h"
 using namespace llvm;
@@ -37,10 +33,9 @@ static LLVMContext &getGlobalContext() { return *GlobalContext; }
 
 struct EnableFunctionOptPass : public FunctionPass {
     static char ID;
-    EnableFunctionOptPass() :FunctionPass(ID) {}
-    bool runOnFunction(Function & F) override {
-        if (F.hasFnAttribute(Attribute::OptimizeNone))
-        {
+    EnableFunctionOptPass() : FunctionPass(ID) {}
+    bool runOnFunction(Function &F) override {
+        if (F.hasFnAttribute(Attribute::OptimizeNone)) {
             F.removeFnAttr(Attribute::OptimizeNone);
         }
         return true;
@@ -51,9 +46,8 @@ char EnableFunctionOptPass::ID = 0;
 
 ///!TODO TO BE COMPLETED BY YOU FOR ASSIGNMENT 3
 struct FuncPtrPass : public ModulePass {
-    static char ID; // Pass identification, replacement for typeid
+    static char ID;  // Pass identification, replacement for typeid
     FuncPtrPass() : ModulePass(ID) {}
-
 
     bool runOnModule(Module &M) override {
         errs() << "Hello: ";
@@ -64,47 +58,44 @@ struct FuncPtrPass : public ModulePass {
     }
 };
 
-
 char FuncPtrPass::ID = 0;
-static RegisterPass<FuncPtrPass> X("funcptrpass", "Print function call instruction");
+static RegisterPass<FuncPtrPass> X("funcptrpass",
+                                   "Print function call instruction");
 
 char Liveness::ID = 0;
 static RegisterPass<Liveness> Y("liveness", "Liveness Dataflow Analysis");
 
-static cl::opt<std::string>
-InputFilename(cl::Positional,
-              cl::desc("<filename>.bc"),
-              cl::init(""));
-
+static cl::opt<std::string> InputFilename(cl::Positional,
+                                          cl::desc("<filename>.bc"),
+                                          cl::init(""));
 
 int main(int argc, char **argv) {
-   LLVMContext &Context = getGlobalContext();
-   SMDiagnostic Err;
-   // Parse the command line to read the Inputfilename
-   cl::ParseCommandLineOptions(argc, argv,
-                              "FuncPtrPass \n My first LLVM too which does not do much.\n");
+    LLVMContext &Context = getGlobalContext();
+    SMDiagnostic Err;
+    // Parse the command line to read the Inputfilename
+    cl::ParseCommandLineOptions(
+        argc, argv,
+        "FuncPtrPass \n My first LLVM too which does not do much.\n");
 
+    // Load the input module
+    std::unique_ptr<Module> M = parseIRFile(InputFilename, Err, Context);
+    if (!M) {
+        Err.print(argv[0], errs());
+        return 1;
+    }
 
-   // Load the input module
-   std::unique_ptr<Module> M = parseIRFile(InputFilename, Err, Context);
-   if (!M) {
-      Err.print(argv[0], errs());
-      return 1;
-   }
-
-   llvm::legacy::PassManager Passes;
+    llvm::legacy::PassManager Passes;
 #if LLVM_VERSION_MAJOR == 5
-   Passes.add(new EnableFunctionOptPass());
+    Passes.add(new EnableFunctionOptPass());
 #endif
-   ///Transform it to SSA
-   Passes.add(llvm::createPromoteMemoryToRegisterPass());
+    /// Transform it to SSA
+    Passes.add(llvm::createPromoteMemoryToRegisterPass());
 
-   /// Your pass to print Function and Call Instructions
-   Passes.add(new Liveness());
-   //Passes.add(new FuncPtrPass());
-   Passes.run(*M.get());
+    /// Your pass to print Function and Call Instructions
+    Passes.add(new Liveness());
+    // Passes.add(new FuncPtrPass());
+    Passes.run(*M.get());
 #ifndef NDEBUG
-   system("pause");
+    system("pause");
 #endif
 }
-
